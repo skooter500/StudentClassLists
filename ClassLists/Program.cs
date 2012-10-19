29014af5caa391fc692c80fac2473f56806d6e61
Bryan Duggan <skooter500@gmail.com>
2012-10-19 16:16:34 +0100

    Bla

--- ClassLists/Program.cs
+++ ClassLists/Program.cs
@@ -12,6 +12,28 @@ namespace ClassLists
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            string line;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file =
+               new System.IO.StreamReader("classlist.txt");
+            System.IO.StreamWriter outFile = new System.IO.StreamWriter("out.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                int i = line.IndexOf("@student.dit.ie");
+                if (i != -1)
+                {
+                    int tab = line.LastIndexOf("\t", i);
+                    string name = line.Substring(tab, (i  - tab) + 15);
+                    name = name.Trim();
+                    outFile.WriteLine(name);
+                    Console.WriteLine(name);
+                }            
+            }
+            Console.WriteLine("All done");
+            Console.ReadLine();
+        }
         }
 
     }
